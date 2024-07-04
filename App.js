fb66f4ebaa402d0a7e653e5cc568216454d69855
Pallavi Rana <Pallavirana989@gmail.com>
2024-07-04 11:33:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import CurrentLocation from "./currentLocation";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <CurrentLocation />
+      </div>
+      <div className="footer-info">
+        <a href="https://www.htmlhints.com/article/how-to-create-toggle-switch/93">
+          
+        </a>{" "}
+        Developed by{" "}
+        <a target="_blank" href="">
+          Pallavi Rana
+        </a>{" "}
+        
+        
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "069e9e0f197522e5bc996b78a2a767ad",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
